4ff47ebe706847cf35592aa9d66eab5aec1f19d9
Teo7297 <teocava7297@gmail.com>
2022-05-24 21:56:44 +0200

    Episode 1: simple triangle

---
+++ src/MyApp/main.cpp
@@ -0,0 +1,48 @@
+//#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#pragma warning( disable : 4244 )
+
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+
+int main(void)
+{
+    GLFWwindow *window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Triangle", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-.5f, -.5f);
+        glVertex2f(.0f, .5f);
+        glVertex2f(.5f, -.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
