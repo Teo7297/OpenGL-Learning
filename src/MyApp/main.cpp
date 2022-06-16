d793f4842d2b3965cf1777d9795a183f01a108c2
Teo7297 <teocava7297@gmail.com>
2022-06-16 22:28:39 +0200

    ImGUI HelloWorld

--- src/MyApp/main.cpp
+++ src/MyApp/main.cpp
@@ -19,6 +19,9 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 int main(void)
 {
@@ -31,7 +34,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                 // openGL v3.3
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // use openGL core profile
 
-    window = glfwCreateWindow(640, 480, "OpenGL Learning Project", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "OpenGL Learning Project", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -55,10 +58,10 @@ int main(void)
         float positions[] =
             {
                 // vertices with texture coords
-                -0.5f, -0.5f, 0.0f, 0.0f, // vertex 0 
-                0.5f, -0.5f, 1.0f, 0.0f,  // vertex 1
-                0.5f, 0.5f, 1.0f, 1.0f,   // vertex 2
-                -0.5f, 0.5f, 0.0f, 1.0f   // vertex 3
+                100.0f, 100.0f, 0.0f, 0.0f, // vertex 0
+                200.0f, 100.0f, 1.0f, 0.0f, // vertex 1
+                200.0f, 200.0f, 1.0f, 1.0f, // vertex 2
+                100.0f, 200.0f, 0.0f, 1.0f  // vertex 3
             };
 
         // Index buffer
@@ -80,17 +83,11 @@ int main(void)
         // Index buffer openGL (ibo = Index Buffer Object)
         IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);  // We create a 4x4 orthographic matrix
-                                                                             // We specify values to adhere to the 4/3 aspect ratio
-                                                                             // An ortho matrix is a way to map coords into a 2D plane where "far" objects do not get smaller.
-                                                                             // It's the opposite of a perspective matrix
-                                                                             //? This is perfect for 2D stuff
-
-
-
-
-
-
+        // MVP matrix
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200.0f, 200.0f, 0.0f));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100.0f, 0.0f, 0.0f));
+        glm::mat4 proj = glm::ortho(-0.0f, 960.0f, -0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 mvp = proj * view * model; //? Mind the reverse order!
 
         // Shaders
         Shader shader("res\\shaders\\Basic.shader");
@@ -104,7 +101,7 @@ int main(void)
         texture.Bind(0);
         shader.SetUniform1i("u_Texture", 0);
 
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         // Unbind everything
         va.Unbind();
@@ -114,19 +111,63 @@ int main(void)
 
         Renderer renderer;
 
+        // ImGUI setup
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO &io = ImGui::GetIO(); 
+        (void)io;
+        ImGui::StyleColorsDark();
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init((char *)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
+
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
         // Render cycle
         while (!glfwWindowShouldClose(window))
         {
             renderer.Clear();
 
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
             shader.Bind();
 
             renderer.Draw(va, ib, shader);
+
+            {
+                static float f = 0.0f;
+                static int counter = 0;
+
+                ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
+
+                ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
+                ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
+                ImGui::Checkbox("Another Window", &show_another_window);
+
+                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
+                ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
+
+                if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
+                    counter++;
+                ImGui::SameLine();
+                ImGui::Text("counter = %d", counter);
+
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+                ImGui::End();
+            }
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
     }
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
 
