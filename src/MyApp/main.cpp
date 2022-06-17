3028908b808a5950048efc1b4945e8ca8c35b744
Teo7297 <teocava7297@gmail.com>
2022-06-17 19:35:34 +0200

    Lesson 18: Simple testing framework

--- src/MyApp/main.cpp
+++ src/MyApp/main.cpp
@@ -23,16 +23,17 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#include <TestClearColor.h>
+
 int main(void)
 {
-    // GLFW boilerplate code
     GLFWwindow *window;
     if (!glfwInit())
         return -1;
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                 // openGL v3.3
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // use openGL core profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     window = glfwCreateWindow(960, 540, "OpenGL Learning Project", NULL, NULL);
     if (!window)
@@ -43,7 +44,6 @@ int main(void)
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); //# Vertical sync / limit fps
 
-    // GLEW boilerplate code
     if (glewInit() != GLEW_OK)
     {
         glfwTerminate();
@@ -52,68 +52,13 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << "\n";
 
-    // Enable blending
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC1_ALPHA));
 
-    { // This scope is a hacky way of fixing the issue of the vertexBuffer being destroyed AFTER the context because its stack allocated
+    {
 
-        // Vertex buffer setup
-        float positions[] =
-            {
-                // vertices with texture coords
-                -50.0f, -50.0f, 0.0f, 0.0f, // vertex 0
-                50.0f, -50.0f, 1.0f, 0.0f,  // vertex 1
-                50.0f, 50.0f, 1.0f, 1.0f,   // vertex 2
-                -50.0f, 50.0f, 0.0f, 1.0f   // vertex 3
-            };
-        
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
-
-
-        // Index buffer setup
-        unsigned int indices[] = {
-            0, 1, 2,
-            2, 3, 0};
-
-        IndexBuffer ib(indices, 6);
-
-
-        // MVP matrix
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-        glm::mat4 proj = glm::ortho(-0.0f, 960.0f, -0.0f, 540.0f, -1.0f, 1.0f);
-
-
-        // Shader initialization
-        Shader shader("res\\shaders\\Basic.shader");
-        shader.Bind();
-
-
-        // Texture initialization
-        Texture texture("res\\textures\\avatar.png");
-        texture.Bind(0);
-
-        shader.SetUniform1i("u_Texture", 0);
-
-
-        // Setup complete, unbind everything
-        va.Unbind();
-        vb.Unbind();
-        ib.Unbind();
-        shader.Unbind();
-
-
-        // Renderer initialization
         Renderer renderer;
 
-
-        // ImGUI setup
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
         ImGuiIO &io = ImGui::GetIO();
@@ -122,67 +67,34 @@ int main(void)
         ImGui_ImplGlfw_InitForOpenGL(window, true);
         ImGui_ImplOpenGL3_Init((char *)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
 
+        test::TestClearColor test;
 
-        // Render cycle variables
-        glm::vec3 translationA(200.0f, 200.0f, 0.0f);
-        glm::vec3 translationB(400.0f, 200.0f, 0.0f);
-
-        // Render cycle
         while (!glfwWindowShouldClose(window))
         {
-            // Clear the renderer buffer
             renderer.Clear();
-            
-            // ImGUI frame rendering
+
+            test.OnUpdate(0.0f);
+            test.OnRender();
+
             ImGui_ImplOpenGL3_NewFrame();
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
-            // Object 1 rendering
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
+            test.OnImGuiRender();
 
-                renderer.Draw(va, ib, shader);
-            }
-            
-            // Object 2 rendering
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-
-            // ImGUI UI composition
-            {
-                ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);
-                ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            }
-
-            // ImGUI rendering
             ImGui::Render();
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-            // GLFW buffer swap
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
     }
 
-    // ImGUI shutdown
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
 
-    // GLFW termination
     glfwTerminate();
 
-    // App termination
     return 0;
 }
