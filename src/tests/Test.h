76456b9fbbc66a140bed7cd8372714a2a7df1041
Teo7297 <teocava7297@gmail.com>
2022-06-20 00:07:32 +0200

    Lesson 19: Dynamic test instancing with small menu

--- src/tests/Test.h
+++ src/tests/Test.h
@@ -1,11 +1,15 @@
 #pragma once
 
-namespace test 
+#include <functional>
+#include <vector>
+#include <string>
+#include <iostream>
+
+namespace test
 {
     class Test
     {
     private:
-        
     public:
         Test() {}
         virtual ~Test() {}
@@ -13,5 +17,25 @@ namespace test
         virtual void OnUpdate(float deltaTime) {}
         virtual void OnRender() {}
         virtual void OnImGuiRender() {}
+    };
+
+    class TestMenu : public Test
+    {
+    public:
+        TestMenu(Test *&currentTestPointer);
+
+        virtual void OnImGuiRender() override;
+
+        template <typename T>
+        void RegisterTest(const std::string &name)
+        {
+            std::cout << "Registering test " << name << std::endl;
+
+            m_Tests.push_back(std::make_pair(name, [](){ return new T(); }));
+        }
+
+    private:
+        Test *&m_CurrentTest;
+        std::vector<std::pair<std::string, std::function<Test *()>>> m_Tests; // This function returns a test pointer to a new test
     };
 }
